9a09dca3c61561a31ce106f12f752354059af736
Nisarg Patel <nisargpatel0330@gmail.com>
2025-12-30 13:34:19 -0500

    Custom Clerk Auth Added

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,4 +1,6 @@
+"use server";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const res = await fetch("http://localhost:8000/");
@@ -8,6 +10,11 @@ export default async function Home() {
     <div className="p-10">
       <h1 className="text-2xl font-bold">Next.js + FastAPI</h1>
       <p className="mt-4">Backend says: {data.message}</p>
+      <div className="mt-6">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <Link href="/">Sign-in</Link>
+        </button>
+      </div>
     </div>
   );
 }
